88e52466f65b600c8b14d48fac074a05265076d9
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-07-08 18:56:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from starter import start
+from nhentai import Nhentai
+from config import bot
+
+try:
+    start()
+    Nhentai()
+    
+except Exception as e:
+    print(e)
+
+bot.start()
+
+bot.run_until_disconnected()
